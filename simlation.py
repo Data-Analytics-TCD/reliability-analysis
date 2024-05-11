9337b1971f79c6e0448e145c88a10f064b194f15
Hi-Im-Jony <jccicai@yahoo.ie>
2024-05-11 17:20:01 +0100

    final simulation code

--- simlation.py
+++ simlation.py
@@ -22,17 +22,11 @@ class Node:
         working = True
         time_accrued = 0
         while time_accrued < sim_time:
-            time_accrued += self.fail() if working else self.repair()
+            time_accrued += np.random.exponential(1 / (self.fr if working else self.rr))
             correct_list = self.fails if working else self.repairs
             correct_list.append(time_accrued)
             working = not working
             self.timeline[time_accrued] = working
-
-    def fail(self):
-        return np.random.exponential(1/self.fr)
-
-    def repair(self):
-        return np.random.exponential(1/self.rr)
 
 class Submatrix:
     def __init__(self, nodes) -> None:
@@ -42,8 +36,10 @@ class Submatrix:
         self.first_repair = -1
         self.fails = []
         self.repairs = []
+        self.total_uptime = 0
+        self.total_downtime = 0
 
-    def process_node_timelines(self):
+    def process_node_timelines(self, sim_time):
         self.timeline = {}
         self.first_fail = -1
         self.first_repair = -1
@@ -58,40 +54,35 @@ class Submatrix:
         sorted_events = sorted(events.items())
         last_state = True
         working_nodes = len(self.nodes)
+        working = True
+        previous_ts = 0
         for ts, event in sorted_events:
             working_nodes += event
             working = working_nodes > 0
             if working != last_state:
                 last_state = working
+                previous_ts = ts
                 self.timeline[ts] = working
-                correct_list = self.repairs if working else self.fails
-                correct_list.append(ts)
-                if not working and self.first_fail == -1:
-                    self.first_fail = ts
-                if working and self.first_repair == -1:
-                    self.first_repair = ts
+                if not working:
+                    self.fails.append(ts)
+                    self.total_uptime += ts - previous_ts
+                    if self.first_fail == -1:
+                        self.first_fail = ts
+                else:
+                    self.repairs.append(ts)
+                    self.total_downtime += ts - previous_ts
+                    if self.first_repair == -1:
+                        self.first_repair = ts
+        if working:
+            self.total_uptime += sim_time - previous_ts
+        else:
+            self.total_downtime += sim_time - previous_ts
 
     def get_uptime_avg(self):
-        if not self.repairs:
-            return 0
-        total_uptime = 0
-        previous_time = 0
-        for time in sorted(self.timeline.keys()):
-            if self.timeline[time]:
-                total_uptime += time - previous_time
-            previous_time = time
-        return total_uptime / (len(self.repairs) + 1)
+        return (self.total_uptime / (len(self.repairs))) if self.repairs else self.total_uptime
 
     def get_downtime_avg(self):
-        if not self.fails:
-            return 0
-        total_downtime = 0
-        previous_time = 0
-        for time in sorted(self.timeline.keys()):
-            if not self.timeline[time]:
-                total_downtime += time - previous_time
-            previous_time = time
-        return total_downtime / len(self.fails)
+        return (self.total_downtime / len(self.fails)) if self.fails else self.total_downtime
 
 class LatticeSystem:
     def __init__(self, m, n, r, s, fail_rate, repair_rate):
@@ -101,6 +92,62 @@ class LatticeSystem:
         self.s = s
         self.fail_rate = fail_rate
         self.repair_rate = repair_rate
+        self.timeline = {}
+        self.total_uptime = 0
+        self.total_downtime = 0
+        self.fails = 0
+        self.repairs = 0
+        self.first_fail = -1
+        self.first_repair = -1
+
+    def process_sm_timelines(self, submatrices, sim_time):
+        self.timeline = {}
+        self.total_uptime = 0
+        self.total_downtime = 0
+        self.fails = 0
+        self.repairs = 0
+        self.first_fail = -1
+        self.first_repair = -1
+        events = defaultdict(int)
+        for sm in submatrices:
+            for fail in sm.fails:
+                events[fail] += 1
+            for repair in sm.repairs:
+                events[repair] -= 1
+        sorted_events = sorted(events.items())
+        last_state = True
+        broken_sm = 0
+        previous_ts = 0
+        working = True
+        for ts, broken in sorted_events:
+            broken_sm += broken
+            working = broken_sm == 0
+            if working != last_state:
+                last_state = working
+                self.timeline[ts] = working
+                if not working:
+                    self.fails += 1
+                    self.total_uptime += ts - previous_ts
+                    if self.first_fail == -1:
+                        self.first_fail = ts
+                else:
+                    self.repairs += 1
+                    self.total_downtime += ts - previous_ts
+                    if self.first_repair == -1:
+                        self.first_repair = ts
+                previous_ts = ts
+        if working:
+            self.total_uptime += sim_time - previous_ts
+        else:
+            self.total_downtime += sim_time - previous_ts
+        print("Uptime:", self.total_uptime, "Downtime:", self.total_downtime)
+    
+    def get_uptime_avg(self):
+        return (self.total_uptime / self.repairs) if self.repairs else self.total_uptime
+
+    def get_downtime_avg(self):
+        return (self.total_downtime / self.fails) if self.fails else self.total_downtime
+        
 
     def simulate(self, sim_time, iterations):
         nodes = [Node(x, y, self.fail_rate, self.repair_rate) for x in range(self.m) for y in range(self.n)]
@@ -111,36 +158,30 @@ class LatticeSystem:
                        for start_col in range(self.n - self.s + 1)]
 
         results = {}
-        for i in range(iterations):
-            start_time = time.time()  # Capture start time of the iteration
-            results[i] = {}
-            for node in nodes:
-                node.generate_timeline(sim_time)
+        with ThreadPoolExecutor() as executor:
+            for i in range(iterations):
+                start_time = time.time()  # Capture start time of the iteration
+                results[i] = {}
+                # Generate timelines for all nodes; waits for completion here
+                list(executor.map(lambda node: node.generate_timeline(sim_time), nodes))
+                # Process node timelines in all submatrices; waits for completion here
+                list(executor.map(lambda sm: sm.process_node_timelines(sim_time), submatrices))
 
-            with ThreadPoolExecutor() as executor:
-                # Execute the processing in parallel
-                list(executor.map(lambda sm: sm.process_node_timelines(), submatrices))
+                self.process_sm_timelines(submatrices, sim_time)
 
-            # Initialize variables to store the lowest first fail and first repair times
-            lowest_first_fail = float('inf')
-            lowest_first_repair = float('inf')
+                end_time = time.time()  # End timing the iteration
+                elapsed_time = end_time - start_time  # Calculate elapsed time
 
-            # Calculate the lowest first fail and first repair times
-            for sm in submatrices:
-                if sm.first_fail != -1:  # Check if a fail time was recorded
-                    lowest_first_fail = min(lowest_first_fail, sm.first_fail)
-                if sm.first_repair != -1:  # Check if a repair time was recorded
-                    lowest_first_repair = min(lowest_first_repair, sm.first_repair)
-
-            end_time = time.time()  # End timing the iteration
-            elapsed_time = end_time - start_time  # Calculate elapsed time
-
-            # Store results in the dictionary
-            results[i]['mttf'] = np.mean([round(sm.get_uptime_avg(), 3) for sm in submatrices])
-            results[i]['mttr'] = np.mean([round(sm.get_downtime_avg(), 3) for sm in submatrices])
-            results[i]['ff'] = (round(lowest_first_fail, 3) if lowest_first_fail != float('inf') else 'No Failure')
-            results[i]['fr'] = (round(lowest_first_repair, 3) if lowest_first_repair != float('inf') else 'No Repair')
-            results[i]['et'] = (round(elapsed_time, 3))
+                # Store results in the dictionary
+                results[i]['mttf'] = self.get_uptime_avg()
+                results[i]['mttr'] = self.get_downtime_avg()
+                results[i]['ff'] = (self.first_fail if self.first_fail != -1 else 'No Failure')
+                results[i]['fr'] = (self.first_repair if self.first_repair != -1 else 'No Repair')
+                results[i]['sm_mttf'] = np.mean([sm.get_uptime_avg() for sm in submatrices])
+                results[i]['sm_mttr'] = np.mean([sm.get_downtime_avg() for sm in submatrices])
+                results[i]['fails'] = self.fails
+                results[i]['repairs'] = self.repairs
+                results[i]['et'] = (round(elapsed_time, 3))
 
         for result in results.items():
             print(result)
@@ -148,38 +189,44 @@ class LatticeSystem:
 
 if __name__ == '__main__':
     # Simulation parameters
-    sim_times = [10, 100]
-    mns = [(3, 3), (10, 10), (50, 50), (100, 100)]
-    rss = [(2, 2), (3, 3), (5, 5), (10, 10)]
-    frs = [(10, 10), (10, 5), (10, 2.5), (10, 1)]
-    with open('simulation_results.csv', 'w', newline='') as file:
-        fieldnames = ['sim_time', '(m,n)', '(r,s)', 'fail_rate', 'repair_rate', 'iteration', 'mttf', 'mttr', 'ff', 'fr', 'elapsed_time']
+    
+    mns = [(3,3), (5,5), (10, 10), (25,25), (30,30), (50, 50) ]
+    rss = [(2,2), (3,3), (5, 5), (8, 8), (10, 10)]
+    frs = [(10, 10), (10, 7.5), (10, 5), (10, 2.5)]
+    sim_times = [50, 75, 100]
+    with open('final_simulation_results.csv', 'w', newline='') as file:
+        fieldnames = ['sim_time', '(m,n)', '(r,s)', 'fail_rate', 'repair_rate', 'iteration', 'mttf', 'mttr', 'ff', 'fr', 'sm_mttf', 'sm_mttr', 'fails', 'repairs', 'elapsed_time']
         writer = csv.DictWriter(file, fieldnames=fieldnames)
         writer.writeheader()
-        for sim_time in sim_times:
-            for mn in mns:
-                m, n = mn
-                for rs in rss:
-                    r, s = rs
-                    for fr in frs:
-                        fr, rr = fr
-                        print("**********************************")
-                        print(f"sim_time: {sim_time}, m: {m}, n: {n}, r: {r}, s: {s}, fr: {fr}, rr: {rr}")
-                        config_key = (sim_time, m, n, r, s, fr, rr)
-                        system = LatticeSystem(m, n, r, s, fr, rr)
-                        results = system.simulate(sim_time, 10)
-                        for iteration, result in results.items():
-                            row = {
-                                'sim_time': sim_time,
-                                '(m,n)': (m,n),
-                                '(r,s)': (r,s),
-                                'fail_rate': fr,
-                                'repair_rate': rr,
-                                'iteration': iteration,
-                                'mttf': result['mttf'],
-                                'mttr': result['mttr'],
-                                'ff': result['ff'],
-                                'fr': result['fr'],
-                                'elapsed_time': result['et']
-                            }
-                            writer.writerow(row)
+        for mn in mns:
+            m, n = mn
+            for rs in rss:
+                r, s = rs
+                for fr in frs:
+                    fr, rr = fr
+                    for sim_time in sim_times:
+                        if not (sim_time > 50 and m > 25):
+                            print("**********************************")
+                            print(f"sim_time: {sim_time}, m: {m}, n: {n}, r: {r}, s: {s}, fr: {fr}, rr: {rr}")
+                            config_key = (sim_time, m, n, r, s, fr, rr)
+                            system = LatticeSystem(m, n, r, s, fr, rr)
+                            results = system.simulate(sim_time, 5)
+                            for iteration, result in results.items():
+                                row = {
+                                    'sim_time': sim_time,
+                                    '(m,n)': (m,n),
+                                    '(r,s)': (r,s),
+                                    'fail_rate': fr,
+                                    'repair_rate': rr,
+                                    'iteration': iteration,
+                                    'mttf': result['mttf'],
+                                    'mttr': result['mttr'],
+                                    'ff': result['ff'],
+                                    'fr': result['fr'],
+                                    'sm_mttf': result['sm_mttf'],
+                                    'sm_mttr': result['sm_mttr'],
+                                    'fails': result['fails'],
+                                    'repairs': result['repairs'],
+                                    'elapsed_time': result['et']
+                                }
+                                writer.writerow(row)
